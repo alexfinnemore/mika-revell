2be34c1cde347cc052440c1044341ce8f748a3ed
Alex Finnemore <alexfinnemore@gmail.com>
2026-01-20 00:36:41 +0100

    Fix TinaCMS build with --skip-cloud-checks flag

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/AlexF/Documents/GitHub/mika-revell/tina/__generated__/.cache/1768865308878', url: 'http://localhost:4001/graphql', token: '4bf988c33e8dd7dd055cfc9e09c19a5373bd7e9e', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '4bf988c33e8dd7dd055cfc9e09c19a5373bd7e9e', queries,  });
 export default client;
   
